7c74736fae97a7b9bcdaa10af4a3a07e1c9afa0c
Juan Cruz Ceballos <94239@sistemas.frc.utn.edu.ar>
2024-09-18 12:54:33 -0300

    backend

---
+++ ED-Backend/src/main/java/com/easydept/edbackend/EdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easydept.edbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdBackendApplication.class, args);
+	}
+
+}
---
+++ ED-Backend/src/test/java/com/easydept/edbackend/EdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydept.edbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
