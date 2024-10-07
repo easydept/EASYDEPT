f62ca9e71ea78561e88517294ef06fdc54cd803c
Valentina Bermudez <95002@sistemas.frc.utn.edu.ar>
2024-10-07 16:29:59 -0300

    repositorios

---
+++ ED-Backend/src/main/java/com/easydept/edbackend/repositories/DepartamentosRepository.java
@@ -0,0 +1,7 @@
+package com.easydept.edbackend.repositories;
+
+import com.easydept.edbackend.entity.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentosRepository extends JpaRepository<Departamento, Long> {
+}
---
+++ ED-Backend/src/main/java/com/easydept/edbackend/repositories/UsuariosRepository.java
@@ -0,0 +1,7 @@
+package com.easydept.edbackend.repositories;
+
+import com.easydept.edbackend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+    public interface UsuariosRepository extends JpaRepository<Usuario, Long> {
+}
