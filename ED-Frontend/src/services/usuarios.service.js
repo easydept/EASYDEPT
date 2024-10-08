40738dde72863810a365a7e3764e43e146bdbb66
Benjamin Castagno <94769@sistemas.frc.utn.edu.ar>
2024-10-08 01:18:30 -0300

    serv

--- ED-Frontend/src/services/usuarios.service.js
+++ ED-Frontend/src/services/usuarios.service.js
@@ -9,6 +9,11 @@ export const postUsuarios = async (usuario) => {
   const response = await axios.post(`http://localhost:8080/usuarios`, usuario)
 };
 
+
+export const getUsuariosPorId = async (id) => {
+  const response = await axios.get(`http://localhost:8080/usuarios/${id}`);
+  return response.data};
+
 export const tieneRolInquilino = async (usuario) => {
     const response = await axios.get(`http://localhost:8080/permisos`, usuario)
     return response.data
